8becff8b0cb599ad96b3aec5b29fe9c47fb29437
Phil Marshall <phillipsolomonmarshall@gmail.com>
2019-07-29 14:30:40 -0500

    add modal to example

--- example/App.jsx
+++ example/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import DurationPicker from "../src/DurationPicker";
+import ReactModal from "react-modal";
+ReactModal.setAppElement("#root");
 function App(props) {
   const [duration, setDuration] = useState({
     hours: 0,
@@ -12,10 +14,29 @@ function App(props) {
   const { hours, minutes, seconds } = duration;
   return (
     <div>
+      <ReactModal
+        isOpen={true}
+        contentLabel="Minimal Modal Example"
+        style={{
+          content: {
+            width: 500,
+            height: 500,
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            marginLeft: -250,
+            marginTop: -250
+          }
+        }}
+      >
+        <div>
+          <DurationPicker onChange={onChange} />
+        </div>
+        <button>Close Modal</button>
+      </ReactModal>
       <div>
         {`You have selected the numbers ${hours}, ${minutes}, and ${seconds}.`}
       </div>
-      <DurationPicker onChange={onChange} />
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,6 @@ module.exports = [
         },
         {
           test: /\.css$/,
-          exclude: /node_modules/,
           use: ["style-loader", "css-loader"]
         }
       ]
