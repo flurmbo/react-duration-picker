cd1cd5e86b7e4082c482ecf81b95b64fe8cc5dd5
Phil Marshall <phillipsolomonmarshall@gmail.com>
2019-08-16 16:41:51 -0500

    add type declarations

---
+++ lib/index.d.ts
@@ -0,0 +1,19 @@
+//import * as React from 'react';
+
+declare module DurationPicker {
+  interface Duration {
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }
+
+  interface Props {
+    maxHours?: number;
+    initialDuration?: DurationPicker.Duration;
+    onChange?: (duration: DurationPicker.Duration) => void;
+  }
+}
+
+declare const DurationPicker: React.FunctionComponent<DurationPicker.Props>;
+
+export default DurationPicker;
